47b9667a830c9514dbac7ebc9e008a758f2ec510
dasha akhten <dasha.akhten@gmail.com>
2020-05-17 10:56:01 -0700

    adding css

--- src/Landing.js
+++ src/Landing.js
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 const Landing = (props) => {
   return (
     <>
-      <h1>PETFUL</h1>
-      <p>Welcome to Petful, the FIFO adoption center.</p>
-      <Link to="/adoption">Click here to get started!</Link>
+      <div className="landing-wrapper">
+        <h1>PETFUL</h1>
+        <p>Welcome to Petful, the FIFO adoption center.</p>
+        <Link to="/adoption">
+          <button type="button">Click here to get started!</button>
+        </Link>
+      </div>
     </>
   );
 };

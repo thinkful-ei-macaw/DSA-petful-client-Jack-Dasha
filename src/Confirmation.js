b5d7a2f5e440aa23ff069e71807ab8fd3b34fd4f
dasha akhten <dasha.akhten@gmail.com>
2020-05-17 11:28:17 -0700

    add confirmation page

--- src/Confirmation.js
+++ src/Confirmation.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import config from './config';
 import './Confirmation.css';
+import { Link } from 'react-router-dom';
 
 class Confirmation extends Component {
   render() {
@@ -9,6 +10,9 @@ class Confirmation extends Component {
         <div className="conf-wrapper">
           <h2>congrats you got a pet!</h2>
           <p>thanks, you're a good person</p>
+          <Link to="/">
+            <button type="button">goodbye</button>
+          </Link>
         </div>
       </div>
     );
